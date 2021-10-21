a94f43c17d02829e1519b1c7d27c91cad9e201b1
pomaskin@rambler.ru <pomaskin@rambler.ru>
2021-10-21 22:36:47 +0500

    update

--- IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/Source.cpp
@@ -22,7 +22,23 @@ public:
 	{
 		this->y = y;
 	}
+	// methods:
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+			return distance;
+	}
 };
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
 
 //#define STRUCT_POINT
 
@@ -44,4 +60,13 @@ void main()
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point B;
+	B.set_x(3);
+	B.set_y(4);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << "Расстояние от точки А до точки В: " << B.distance(A) << endl;
+	cout << "Расстояние между точками А и В: " << distance(A, B) << endl;
+	cout << "Расстояние между точками В и А: " << distance(B, A) << endl;
 }
