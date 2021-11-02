a670eb2ec4c4566ff7fdcc4905b6f8be3c4f5df3
pomaskin@rambler.ru <pomaskin@rambler.ru>
2021-11-02 18:03:26 +0500

    update

--- Fraction/Source.cpp
+++ Fraction/Source.cpp
@@ -1,5 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 class Fraction
 {
@@ -81,6 +84,7 @@ public:
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
+	
 
 	// Methods
 	void to_proper()
@@ -112,7 +116,79 @@ public:
 		else if (integer == 0)cout << 0;
 		cout << endl;
 	}
+	Fraction& operator++()
+	{
+		integer++;
+		return *this;
+	}
+	Fraction operator++(int)
+	{
+		Fraction buffer = *this;
+		integer++;
+		return buffer;
+	}
+	Fraction& operator--()
+	{
+		integer--;
+		return *this;
+	}
+	Fraction operator--(int)
+	{
+		Fraction buffer = *this;
+		integer--;
+		return buffer;
+	}
 };
+
+ostream& operator<<(ostream& os, const Fraction& obj)
+{
+	if (obj.get_integer())cout << obj.get_integer();
+	if (obj.get_numerator())
+	{
+		if (obj.get_integer())cout << "(";
+		cout << obj.get_numerator() << "/" << obj.get_denominator();
+		if (obj.get_integer())cout << ")";
+	}
+	else if (obj.get_integer() == 0)cout << 0;
+	cout << endl;
+	return os;
+}
+
+istream& operator>>(istream& is, Fraction& obj)
+{
+	int integer, numerator, denominator;
+	is >> integer;
+	is >> numerator;
+	is >> denominator;
+	obj.set_integer(integer);
+	obj.set_numerator(numerator);
+	obj.set_denominator(denominator);
+	return is;
+}
+
+Fraction operator*(Fraction& left, Fraction& right)
+{
+	left.to_improper();
+	right.to_improper();
+	return Fraction(left.get_numerator() * right.get_numerator(), left.get_denominator() * right.get_denominator());
+}
+
+Fraction operator/(Fraction& left, Fraction& right)
+{
+	left.to_improper();
+	right.to_improper();
+	return Fraction(left.get_numerator() / right.get_numerator(), left.get_denominator() / right.get_denominator());
+}
+
+Fraction operator+(const Fraction& left, const Fraction& right)
+{
+	return Fraction(left.get_integer() + right.get_integer(), left.get_numerator() * right.get_denominator() + right.get_numerator() * left.get_denominator(), left.get_denominator() * right.get_denominator());
+}
+
+Fraction operator-(const Fraction& left, const Fraction& right)
+{
+	return Fraction(left.get_integer() - right.get_integer(), left.get_numerator() * right.get_denominator() - right.get_numerator() * left.get_denominator(), left.get_denominator() * right.get_denominator());
+}
 
 //#define CONSTRUCTORS_CHECK
 
@@ -130,9 +206,31 @@ void main()
 	D.print();
 #endif // CONSTRUCTORS_CHECK
 
-	Fraction A(14, 4);
-	A.to_proper();
-	A.print();
-	A.to_improper();
-	A.print();
+	//Fraction A(14, 4);
+	//A.to_proper();
+	////A.print();
+	//cout << A << endl;
+	//A.to_improper();
+	////A.print();
+	//cout << A << endl;
+	Fraction A(4, 8);
+	Fraction B(1, 3);
+	Fraction C = A * B;
+	cout << C << endl;
+	Fraction D = A / B;
+	cout << D << endl;
+	Fraction E = A + B;
+	cout << E << endl;
+	Fraction F = A - B;
+	cout << F << endl;
+	Fraction G(1, 3);
+	G++;
+	cout << G << endl;
+	Fraction H(3, 1, 3);
+	H--;
+	cout << H << endl;
+	Fraction J;
+	cout << "Введите значения дроби: ";
+	cin >> J;
+	cout << J << endl;
 }
