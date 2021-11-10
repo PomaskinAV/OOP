963a27525e332afc1927d7965bceb1169c876310
pomaskin@rambler.ru <pomaskin@rambler.ru>
2021-11-10 22:21:26 +0500

    update

--- String/Source.cpp
+++ String/Source.cpp
@@ -6,14 +6,59 @@ using std::endl;
 
 class String
 {
-	int size;
-	char* str;
+	int size; //размер строки в байтах
+	char* str; //адрес строки в динамичесокй памяти
 public:
+	//	Constructor
+	String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//			Operators:
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssigment:\t\t" << this << endl;
+		return *this;
+	}
+
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+
 	int get_size()const
 	{
 		return size;
 	}
-	char* get_str()const
+	const char* get_str()const
 	{
 		return str;
 	}
@@ -25,36 +70,34 @@ public:
 	{
 		this->str = str;
 	}
-	//	Constructor
-	String()
-	{
-		this->size = 80;
-		this->str = new char[size] {};
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	~String()
-	{
-		delete[] str;
-		cout << "Destructor:\t" << this << endl;
-	}
-
-	//			Operators:
-
+	
 };
 
 ostream& operator<<(ostream& os, const String& obj)
 {
-	os << obj.get_str();
-	return os;
+	return os << obj.get_str();
 }
+
+//#define CONSTRUCTOR_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef CONSTRUCTOR_CHECK
+	String str;
+	str.print();
+	String str1 = "Hello";
+	//str1.print();
+	cout << str1 << endl;
+
+	String str2 = str1;
+	cout << str2 << endl;
+	String str3;
+	str3 = str2;
+	cout << str3 << endl;
+#endif // CONSTRUCTOR_CHECK
 
 	String str1 = "Hello";
-	String str2 = "World";
-	String str3;
-	str3 = str1 + str2;
-	cout << str3 << endl;
+	str1 = str1;
+	cout << str1 << endl;
 }
